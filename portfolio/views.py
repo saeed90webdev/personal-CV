6655e09fbd64e605e728824b4a6b295ea2ce4dfc
saeed90webdev <saeed90.webdeveloper@gmail.com>
2025-05-02 12:06:46 +0330

    data submission via render

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,13 +1,34 @@
 from django.shortcuts import render
 
 def index_view(request):
-    return render(request, 'index.html')
+    context = {
+        'full_name' : 'Saeid Salehabadi',
+        }
+    return render(request, 'index.html', context)
 
 def about_view(request):
-    return render(request, 'about.html')
+    context = {
+        'full_name' : 'Saeid Salehabadi',
+        'Date_of_Birth' : 'February 1, 1990',
+        'website' : 'www.example.com',
+        'phone' : '+98 910 061 8961',
+        'email' : 'saeidsalehabadi.dev@gmail.com',
+        'location' : 'Mashhad, Razavi Khorasan, Iran',
+        'age' : '35',
+        'degree' : 'Bachelor of Nursing',
+        'freelance_status' : 'Available',
+        'languages' : 'Persian, English',
+        }
+    return render(request, 'about.html', context)
 
 def resume_view(request):
-    return render(request, 'resume.html')
+    context = {
+        'full_name' : 'Saeid Salehabadi',
+        'location' : 'Mashhad, Razavi Khorasan, Iran',
+        'phone' : '+98 910 061 8961',
+        'email' : 'saeidsalehabadi.dev@gmail.com',
+        }
+    return render(request, 'resume.html', context)
 
 def services_view(request):
     return render(request, 'services.html')
@@ -16,4 +37,9 @@ def portfolio_view(request):
     return render(request, 'portfolio.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    context = {
+        'location' : 'Mashhad, Razavi Khorasan, Iran',
+        'phone' : '+98 910 061 8961',
+        'email' : 'saeidsalehabadi.dev@gmail.com',
+        }
+    return render(request, 'contact.html', context)
